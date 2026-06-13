e867db579b967165b6cc195030a5ac1c2cfa455d
anjlishivhare <sakshishivhare0909@gmai.com>
2026-06-13 21:15:19 +0530

    Day-12 uploaded.

---
+++ day 12/funcforpalindrome.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int palindrome(int n)
+{
+    int rev = 0, temp = n;
+
+    while(temp > 0)
+    {
+        rev = rev * 10 + temp % 10;
+        temp = temp / 10;
+    }
+
+    if(rev == n)
+        return 1;
+    else
+        return 0;
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if(palindrome(n))
+        printf("Palindrome Number");
+    else
+        printf("Not Palindrome Number");
+
+    return 0;
+}
